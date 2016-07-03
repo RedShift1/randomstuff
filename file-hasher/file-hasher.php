6d6ed48733d77c7678e33ec6ab25022c164f4bc1
Glenn Matthys <glenn@webmind.be>
2016-07-03 23:21:17 +0200

    Add performance measurements

--- file-hasher/file-hasher.php
+++ file-hasher/file-hasher.php
@@ -100,6 +100,88 @@ class DB
     }
 }
 
+class IOMeasureResult
+{
+    public $readBps;
+    public $writeBps;
+
+    public function __construct($readBps, $writeBps)
+    {
+        $this->readBps  = $readBps;
+        $this->writeBps = $writeBps;
+    }
+}
+
+class IOMeasure
+{
+    private $myPid;
+    private $time;
+    private $readBytes;
+    private $writeBytes;
+
+    /**
+     * @return int
+     */
+    public function getReadBytes() { return $this->readBytes; }
+
+    /**
+     * @return int
+     */
+    public function getWriteBytes() { return $this->writeBytes; }
+
+    /**
+     * @return mixed
+     */
+    public function getTime() { return $this->time; }
+
+    public function __construct()
+    {
+        $this->myPid        = getmypid();
+        $this->time         = microtime(true);
+
+        $measurements       = file_get_contents("/proc/{$this->myPid}/io");
+        preg_match('#rchar: (\d+)\nwchar: (\d+)#', $measurements, $matches);
+        $this->readBytes    = (int) $matches[1];
+        $this->writeBytes   = (int) $matches[2];
+    }
+
+    public function stat(\IOMeasure $other)
+    {
+        $readBps    = 0;
+        $writeBps   = 0;
+
+        if($other->getTime() > $this->getTime())
+        {
+            $a = $other;
+            $b = $this;
+        }
+        else
+        {
+            $a = $this;
+            $b = $other;
+        }
+
+        if(($timeDelta  = $a->getTime() - $b->getTime()) > 0)
+        {
+            $readBps = ($a->getReadBytes() - $b->getReadBytes()) / $timeDelta;
+            $writeBps = ($a->getWriteBytes() - $b->getWriteBytes()) / $timeDelta;
+        }
+
+        return new \IOMeasureResult($readBps, $writeBps);
+    }
+}
+
+function formatBytes($bytes, $precision = 2)
+{
+    $units = array('B', 'KiB', 'MiB', 'GiB', 'TiB');
+
+    $bytes  = max($bytes, 0);
+    $pow    = floor(($bytes ? log($bytes) : 0) / log(1024));
+    $pow    = min($pow, count($units) - 1);
+    $bytes  /= pow(1024, $pow);
+
+    return round($bytes, $precision) . ' ' . $units[$pow];
+}
 
 $last_line_length = 0;
 
@@ -150,10 +232,6 @@ function build_tree($cur_dir)
 }
 
 $options = getopt('d:s:');
-$hash_added_cnt = 0;
-$hash_ok_cnt    = 0;
-$hash_fail_cnt  = 0;
-$hash_fails     = array();
 
 if($options === false || !array_key_exists('d', $options) || !array_key_exists('s', $options))
 {
@@ -173,6 +251,17 @@ if(!is_dir($dir))
     exit(64);
 }
 
+
+$hash_added_cnt = 0;
+$hash_ok_cnt    = 0;
+$hash_fail_cnt  = 0;
+$percent        = 0;
+$hash_fails     = array();
+
+$stat_begin     = new \IOMeasure();
+$stat_now       = new \IOMeasure();
+$stat_result    = $stat_now->stat($stat_begin);
+
 echo "[{$dbfile}] Initializing database... ";
 
 $db = new \DB($dbfile);
@@ -186,13 +275,29 @@ $count  = count($tree);
 
 echo "{$count} files.\n";
 
+
 for($i = 0; $i < $count; $i++)
 {
     $human_index    = $i + 1;
     $percent        = str_pad(floor(($human_index / $count) * 100), 3, "0", STR_PAD_LEFT);
     $basename       = basename($tree[$i]);
 
-    echo_replace("[{$dir}] [{$percent}%] [OK:{$hash_ok_cnt}, ADDED:{$hash_added_cnt}, FAIL:{$hash_fail_cnt}] Hashing file {$human_index} of {$count} ({$basename})\r");
+    $output = sprintf(
+        '[%s] [%s%%] [OK:%d, ADDED: %d, FAIL: %d] [READ: %s, %sps] Hashing file %d/%d (%s)',
+        $dir,
+        $percent,
+        $hash_ok_cnt,
+        $hash_added_cnt,
+        $hash_fail_cnt,
+        formatBytes($stat_now->getReadBytes()),
+        formatBytes($stat_result->readBps),
+        $human_index,
+        $count,
+        $basename
+    );
+
+    echo_replace($output);
+
     $hash           = md5_file($tree[$i]);
 
     switch($db->verifyHash($tree[$i], $hash))
@@ -210,9 +315,23 @@ for($i = 0; $i < $count; $i++)
             $hash_fail_cnt++;
             break;
     }
+
+    $stat_now       = new \IOMeasure();
+    $stat_result    = $stat_now->stat($stat_begin);
 }
 
-echo_replace("[{$dir}] [{$percent}%] [OK:{$hash_ok_cnt}, ADDED:{$hash_added_cnt}, FAIL:{$hash_fail_cnt}]\r");
+$output = sprintf(
+    '[%s] [%s%%] [OK:%d, ADDED: %d, FAIL: %d] [READ: %s, %sps]',
+    $dir,
+    $percent,
+    $hash_ok_cnt,
+    $hash_added_cnt,
+    $hash_fail_cnt,
+    formatBytes($stat_now->getReadBytes()),
+    formatBytes($stat_result->readBps)
+);
+
+echo_replace($output);
 echo "\n";
 
 for($i = 0; $i < count($hash_fails); $i++)
